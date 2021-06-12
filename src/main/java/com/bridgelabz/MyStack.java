3d4939ab92f3fe105d63deddbadeda43f28cff23
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-12 20:45:41 +0530

    ADDED:Peak and pop from stack

--- src/main/java/com/bridgelabz/MyStack.java
+++ src/main/java/com/bridgelabz/MyStack.java
@@ -2,18 +2,27 @@ package com.bridgelabz;
 
 public class MyStack<K> {
     public final MyLinkedList<K> myLinkedList;
+
     public MyStack()
     {
         myLinkedList = new MyLinkedList<>();
     }
+
     public void push(INode<K> element)
     {
         myLinkedList.add(element);
     }
+
     public INode<K> peak()
     {
         return myLinkedList.head;
     }
+
+    public INode<K> pop()
+    {
+        return myLinkedList.pop();
+    }
+
     public void printStack()
     {
         myLinkedList.printMyNodes();
