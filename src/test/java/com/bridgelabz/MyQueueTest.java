2d974bb72b091b69ef83b9ad74a1e77a1d621f0d
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-16 11:27:41 +0530

    ADDED:Create Queue

---
+++ src/test/java/com/bridgelabz/MyQueueTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+    @Test
+    public void given3Numbers_InQueueWhenAdded_ShouldPassQueueTest() {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        INode<Integer> myNode = myQueue.peak();
+        myQueue.printQueue();
+        Assert.assertEquals(myNode, myFirstNode);
+    }
+}
